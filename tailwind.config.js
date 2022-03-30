899092add098cd654bab8a52cb9b54c10950c836
MrFellox <jfernandohernandez28@gmail.com>
2022-03-30 16:11:07 -0600

    refactored navbar with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pymanager/templates/**/*.html",
+  ],
+  theme: {
+    extend: {
+      "colors": {
+        "body-bg": "#343a40",
+        "navbar-bg": "#212529",
+      }
+    },
+  },
+  plugins: [],
+}
