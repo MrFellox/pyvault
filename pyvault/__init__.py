3a8e2782b28040b2dd791e86e4705f5b0985fefa
MrFellox <jfernandohernandez28@gmail.com>
2022-04-07 17:55:06 -0600

    App now stores sensitive data in a .env file

--- pyvault/__init__.py
+++ pyvault/__init__.py
@@ -2,18 +2,23 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
+import os
+
+# Get data from .env file
+load_dotenv()
+print('Secret key found: ' + os.getenv('SECRET_KEY'))
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pyvault.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.templates_auto_reload = True
-app.secret_key = '23595366b265957cdebfd34a57b253a3'
+app.secret_key = os.getenv('SECRET_KEY')
 
 bcrypt = Bcrypt(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 db = SQLAlchemy(app)
-
 
 
 from pyvault.routes import *
